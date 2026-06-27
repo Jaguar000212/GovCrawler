b531be10234e990e4c2f927960c2bcac44d4c3a3
Shryansh Parashar <Jaguar000212@gmail.com>
2026-06-27 20:42:10 +0530

    refact: removed test files which are no longer needed

--- test_list_campaigns.py
+++
@@ -1,8 +0,0 @@
-from portal.db.models import Database
-import traceback
-
-try:
-    db = Database({"database": {"uri": "sqlite:///portal/data/mishacrawler.db"}})
-    print(db.list_campaigns())
-except Exception as e:
-    traceback.print_exc()
